4bd03053dc2d51f6317709474cc7d2787197e63f
Yunus Emre Karaman <emre884k@gmail.com>
2026-05-10 18:49:21 +0300

    learn: add book service interface

--- internal/services/book_services.go
+++ internal/services/book_services.go
@@ -1,1 +1,11 @@
 package services
+
+import "BookHub/internal/models"
+
+type BookServices interface {
+	GetAllBooks() ([]models.Book, error)
+	GetBookByID(id int) (*models.Book, error)
+	CreateBook(book models.Book) (models.Book, error)
+	UpdateBook(id int, book models.Book) (models.Book, error)
+	DeleteBook(id int) error
+}
