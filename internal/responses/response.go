8843025b3477a770d5d9b8b1d8690f5cf09d6a9b
Yunus Emre Karaman <emre884k@gmail.com>
2026-05-10 18:51:31 +0300

    learn: add response helper functions

--- internal/responses/response.go
+++ internal/responses/response.go
@@ -1,7 +1,34 @@
 package responses
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type APIResponse struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
+}
+
+func JSON(w http.ResponseWriter, statusCode int, response APIResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func Success(w http.ResponseWriter, statusCode int, message string, data interface{}) {
+	JSON(w, statusCode, APIResponse{
+		Success: true,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func Error(w http.ResponseWriter, statusCode int, message string) {
+	JSON(w, statusCode, APIResponse{
+		Success: false,
+		Message: message,
+	})
 }
