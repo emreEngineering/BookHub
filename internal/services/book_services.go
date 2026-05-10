3d2b904536d3f1ba99b1050f344b4402240505ca
Yunus Emre Karaman <emre884k@gmail.com>
2026-05-10 18:49:51 +0300

    learn: move book validation to service

--- internal/services/book_services.go
+++ internal/services/book_services.go
@@ -3,6 +3,7 @@ package services
 import (
 	"BookHub/internal/models"
 	"BookHub/internal/repositories"
+	"errors"
 )
 
 type BookServices interface {
@@ -21,4 +22,44 @@ func NewBookService(bookRepo repositories.BookRepository) *DefaultBookService {
 	return &DefaultBookService{
 		bookRepo: bookRepo,
 	}
+}
+
+func (s *DefaultBookService) GetAllBooks() ([]models.Book, error) {
+	return s.bookRepo.FindAll()
+}
+
+func (s *DefaultBookService) GetBookByID(id int) (*models.Book, error) {
+	return s.bookRepo.FindByID(id)
+}
+
+func (s *DefaultBookService) CreateBook(book models.Book) (models.Book, error) {
+	if err := validateBook(book); err != nil {
+		return models.Book{}, err
+	}
+
+	return s.bookRepo.Create(book)
+}
+
+func (s *DefaultBookService) UpdateBook(id int, book models.Book) (models.Book, error) {
+	if err := validateBook(book); err != nil {
+		return models.Book{}, err
+	}
+
+	return s.bookRepo.Update(id, book)
+}
+
+func (s *DefaultBookService) DeleteBook(id int) error {
+	return s.bookRepo.Delete(id)
+}
+
+func validateBook(book models.Book) error {
+	if book.Title == "" {
+		return errors.New("Kitap adı boş olamaz")
+	}
+
+	if book.Author == "" {
+		return errors.New("Yazar adı boş olamaz")
+	}
+
+	return nil
 }
