a5f90247341fa1b1bd45bd0f406526cad675e6f8
Yunus Emre Karaman <emre884k@gmail.com>
2026-05-10 18:44:02 +0300

    learn: add books list endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"BookHub/internal/repositories"
 	"fmt"
 	"net/http"
 )
+
+var bookRepo repositories.BookRepository
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "BookHub calisiyor")
@@ -17,10 +20,30 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "BookHub bir kitap yonetim sistemidir")
 }
 
+func booksHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Bu endpoint sadece GET destekler", http.StatusMethodNotAllowed)
+		return
+	}
+
+	books, err := bookRepo.FindAll()
+	if err != nil {
+		http.Error(w, "Kitaplar alinamadi", http.StatusInternalServerError)
+		return
+	}
+
+	for _, book := range books {
+		fmt.Fprintf(w, "%d - %s / %s (%d)\n", book.ID, book.Title, book.Author, book.Year)
+	}
+}
+
 func main() {
+	bookRepo = repositories.NewMemoryBookRepository()
+
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/books", booksHandler)
 
 	fmt.Println("Server calisiyor: http://localhost:8080")
 
